39f29bb2feba1245b15c8802e640c09ae3ae6b07
Charles-Zhong <zhongzhehua@outlook.com>
2022-06-28 14:01:34 +0800

    add nni resume

--- nni.sh
+++ nni.sh
@@ -7,3 +7,13 @@ if [ "$1" = "stop" ];
 then
     nnictl stop --all
 fi
+
+if [ "$1" = "resume" ];
+then
+    if [ "$2" = "" ];
+    then
+        echo "error: Miss Experiment ID"
+    else
+        nnictl resume $2
+    fi
+fi
