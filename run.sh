68910dcd9d0707be0c5edfc63a9a1f54c340c354
Charles-Zhong <zhongzhehua@outlook.com>
2022-07-05 18:02:34 +0800

    update log_path

--- run.sh
+++ run.sh
@@ -2,8 +2,6 @@
 #                    [--model_name {bert-base-uncased,roberta-base,roberta-large}] [--seed SEED]
 #                    [--batch_size BATCH_SIZE] [--epochs EPOCHS] [--adv_init_epsilon ADV_INIT_EPSILON] [--adv_init_type ADV_INIT_TYPE]
 #                    [--sampling_times_theta SAMPLING_TIMES_THETA] [--sampling_times_delta SAMPLING_TIMES_DELTA]
-#                    [--sampling_noise_theta SAMPLING_NOISE_THETA] [--sampling_noise_delta SAMPLING_NOISE_DELTA]
 #                    [--sampling_step_theta SAMPLING_STEP_THETA] [--sampling_step_delta SAMPLING_STEP_DELTA]
+#                    [--sampling_noise_theta SAMPLING_NOISE_THETA] [--sampling_noise_delta SAMPLING_NOISE_DELTA]
 #                    [--lambda_s LAMBDA_S] [--beta_s BETA_S] [--beta_p BETA_P] [--save_model SAVE_MODEL]
-
-python GLUE-MAT.py
