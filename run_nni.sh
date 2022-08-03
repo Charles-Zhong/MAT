0a74d27727a09d172e31f940e3c73a40cfe8af56
Charles-Zhong <zhongzhehua@outlook.com>
2022-08-03 19:14:26 +0800

    add nni experiment_name

--- run_nni.sh
+++ run_nni.sh
@@ -1,6 +1,13 @@
 if [ "$1" = "run" ];
 then
-    nnictl create --config nni/config.yaml --port 8080
+    if [ "$2" = "" ];
+    then
+        echo "error: Miss Experiment Name"
+    else
+        sed -i "1d" nni/config.yaml
+        sed -i "1i experiment_name: $2" nni/config.yaml
+        nnictl create --config nni/config.yaml --port 8080
+    fi
 fi
 
 if [ "$1" = "stop" ];
